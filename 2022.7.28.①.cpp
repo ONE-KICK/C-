5ce402ed8da363cc86d46ec2c1826e7ec29b9cae
ONE-KICK <82807718+ONE-KICK@users.noreply.github.com>
2022-07-28 13:18:48 +0800

    不用加减乘除法做加法

---
+++ 2022.7.28.①.cpp
@@ -0,0 +1,14 @@
+int Add(int num1, int num2)
+{
+    int sum, forward;
+    do
+    {
+        sum = num1 ^ num2;
+        forward = (num1 & num2) << 1;
+        num1 = sum;
+        num2 = forward;
+
+    } while (num2 != 0);
+
+    return num1;
+}
