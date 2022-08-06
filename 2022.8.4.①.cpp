7d5bcb05c2267f861cf3e33584ab876649ef03cd
ONE-KICK <82807718+ONE-KICK@users.noreply.github.com>
2022-08-06 09:57:50 +0800

    客似云来 牛客

---
+++ 2022.8.4.①.cpp
@@ -0,0 +1,25 @@
+#include<iostream>   //¿ÍËÆÔÆÀ´ Å£¿Í
+#include<vector>
+using namespace std;
+int main()
+{
+	int from, to;
+	long long count;
+	while (cin >> from >> to)
+	{
+		count = 0;
+		vector<long long>v(80, 0);
+		v[0] = 1;
+		v[1] = 1;
+		for (int i = 2; i < 80; i++)
+		{
+			v[i] = v[i - 2] + v[i - 1];
+		}
+		for (int i = from - 1; i < to; ++i)
+		{
+			count += v[i];
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
