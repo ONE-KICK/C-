411b31d9ffc9c702181ec4058b11da6a72d77645
ONE-KICK <82807718+ONE-KICK@users.noreply.github.com>
2022-08-06 10:03:50 +0800

    养兔子 牛客

---
+++ 2022.8.5.①.cpp
@@ -0,0 +1,18 @@
+#include<iostream>  //ÑøÍÃ×Ó Å£¿Í
+#include<vector>
+using namespace std;
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		vector<long long>v(91, 0);
+		v[1] = 1;
+		v[2] = 1;
+		for (int i = 3; i <= 90; ++i)
+		{
+			v[i] = v[i - 2] + v[i - 1];
+		}
+		cout << v[n] << endl;
+	}return 0;
+}
