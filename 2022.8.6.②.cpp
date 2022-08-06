a7eb7c632d147a390d62546cd39f442ca5e2f9ec
ONE-KICK <82807718+ONE-KICK@users.noreply.github.com>
2022-08-06 11:34:18 +0800

    抄送列表 牛客

---
+++ 2022.8.6.②.cpp
@@ -0,0 +1,37 @@
+#include<iostream>  //³­ËÍÁĞ±í Å£¿Í
+#include<string>
+#include<algorithm>
+#include<vector>
+using namespace std;
+int main()
+{
+	string str, name;
+	while (getline(cin,str))
+	{
+		getline(cin, name);
+		vector<string>v;
+		for (int i = 0; i < str.size(); ++i)
+		{
+			if (str[i] == '"')
+			{
+				v.push_back(str.substr(i + 1, str.find('"', i + 1) - i - 1));
+				i = str.find('"', i + 1) + 1;
+			}
+			else
+				{
+					if (str.find(',', i) == str.npos)
+					{
+					v.push_back(str.substr(i, str.size() - i));
+					i = str.size();
+					}
+					else
+					{
+						v.push_back(str.substr(i, str.find(',') - i));
+						i = str.find(',', i);
+					}
+				}
+		}
+		cout << ((find(v.begin(), v.end(), name) != v.end()) ? "Ignore" : "Important!") << endl;
+	}
+	return 0;
+}
